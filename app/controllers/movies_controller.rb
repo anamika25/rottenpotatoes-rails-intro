670d47e3e74d8e02289162e959e6697f4cfda15a
Anamika <anamika@anamika.suddenlink.net>
2015-09-23 01:29:25 -0500

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,18 +13,60 @@ class MoviesController < ApplicationController
   def index
     #@movies = Movie.all
 @movies= Movie.order(sort_column + " "+ sort_direction)
+
 if params[:sort]=="title"
 	@title_header="hilite"
    elsif params[:sort]=="release_date"
 	@release_date_header="hilite"
 end
-  end
+
+@all_ratings = Movie.ratings
+if(params[:ratings] == nil)
+@selected_ratings = @all_ratings
+else
+@selected_ratings = params[:ratings].keys
+end
+if(params[:sort] != nil)
+@movies = Movie.where(:rating =>@selected_ratings).order(params[:sort])
+else
+@movies = Movie.where(:rating =>@selected_ratings)
+end
+end
+
 def sort_column
  Movie.column_names.include?(params[:sort]) ? params[:sort] : "title"
 end
 
 def sort_direction
  %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+end
+
+def new
+# default: render 'new' template
+end
+
+def create
+@movie = Movie.create!(params[:movie])
+flash[:notice] = "#{@movie.title} was successfully created."
+redirect_to movies_path
+end
+
+def edit
+@movie = Movie.find params[:id]
+end
+
+def update
+@movie = Movie.find params[:id]
+@movie.update_attributes!(params[:movie])
+flash[:notice] = "#{@movie.title} was successfully updated."
+redirect_to movie_path(@movie)
+end
+
+def destroy
+@movie = Movie.find(params[:id])
+@movie.destroy
+flash[:notice] = "Movie '#{@movie.title}' deleted."
+redirect_to movies_path
 end
 
   def new
