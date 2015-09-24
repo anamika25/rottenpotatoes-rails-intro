a0d609adbde4daf04edaf61d0828391cbecda532
Anamika <bedi231@tamu.edu>
2015-09-23 20:41:48 -0500

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,29 @@ class MoviesController < ApplicationController
 
   def index
     #@movies = Movie.all
-@movies= Movie.order(sort_column + " "+ sort_direction)
+
+
+@all_ratings = Movie.ratings
+
+if (params[:ratings])
+@selected = params[:ratings].keys
+session[:ratings] = params[:ratings]
+elsif (session[:ratings])
+flag = 1
+@selected = session[:ratings].keys
+else
+@selected = @all_ratings
+end
+
+if (params[:sort])
+@sort_method = params[:sort]
+session[:sort] = params[:sort]
+elsif (session[:sort])
+flag = 1
+@sort_method = session[:sort]
+else
+@sort_method = nil
+end
 
 if params[:sort]=="title"
 	@title_header="hilite"
@@ -20,54 +42,12 @@ if params[:sort]=="title"
 	@release_date_header="hilite"
 end
 
-@all_ratings = Movie.ratings
-if(params[:ratings] == nil)
-@selected_ratings = @all_ratings
-else
-@selected_ratings = params[:ratings].keys
+if flag == 1
+redirect_to movies_path(:ratings => session[:ratings], :sort => session[:sort])
 end
-if(params[:sort] != nil)
-@movies = Movie.where(:rating =>@selected_ratings).order(params[:sort])
-else
-@movies = Movie.where(:rating =>@selected_ratings)
-end
+@movies = Movie.where(:rating => @selected).order(@sort_method)
 end
 
-def sort_column
- Movie.column_names.include?(params[:sort]) ? params[:sort] : "title"
-end
-
-def sort_direction
- %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-end
-
-def new
-# default: render 'new' template
-end
-
-def create
-@movie = Movie.create!(params[:movie])
-flash[:notice] = "#{@movie.title} was successfully created."
-redirect_to movies_path
-end
-
-def edit
-@movie = Movie.find params[:id]
-end
-
-def update
-@movie = Movie.find params[:id]
-@movie.update_attributes!(params[:movie])
-flash[:notice] = "#{@movie.title} was successfully updated."
-redirect_to movie_path(@movie)
-end
-
-def destroy
-@movie = Movie.find(params[:id])
-@movie.destroy
-flash[:notice] = "Movie '#{@movie.title}' deleted."
-redirect_to movies_path
-end
 
   def new
     # default: render 'new' template
